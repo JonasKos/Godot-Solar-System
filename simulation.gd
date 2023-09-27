5e168f38099cf24ce9efb7daec23a93fd7cae24a
JonasKos <jonas.kosmo@gmail.com>
2023-09-27 22:34:28 +0200

    sun is now affected by the distruction of the universe

--- simulation.gd
+++ simulation.gd
@@ -1,7 +1,9 @@
 extends Node3D
 
 var firepoint
-const FORCE = 100
+var tracersEnabled = true
+var sunAmount = 0
+const FORCE = 500
 # Called when the node enters the scene tree for the first time.
 
 func _ready():
@@ -18,14 +20,22 @@ func _process(delta):
 			if otherbody.name == "SunBody":
 				otherbody.mass -= 10.0
 	
-	if Input.is_mouse_button_pressed(1):
+	if Input.is_mouse_button_pressed(1) && sunAmount < 30:
+		get_node("SunBody").is_sun = false
 		var bullet = preload("res://bullet.tscn").instantiate()
 		var level = get_node(".")
 		level.add_child(bullet)
 		bullet.set_global_transform(firepoint.global_transform)
 		bullet.apply_central_impulse(firepoint.global_transform.basis.z * -FORCE)
+		var children = get_all_children(level)
+		for child in children:
+			if child is CPUParticles3D:
+				child.visible = false
+		sunAmount += 1
 		
 		
-		
-		
-		
+func get_all_children(in_node,arr:=[]):
+	arr.push_back(in_node)
+	for child in in_node.get_children():
+		arr = get_all_children(child,arr)
+	return arr
