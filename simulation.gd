67fdabad2ef0742f908f8e3e1cb992eed1061e59
JonasKos <jonas.kosmo@gmail.com>
2023-09-27 20:43:07 +0200

    added the possibility of destroying the universe

--- simulation.gd
+++ simulation.gd
@@ -1,10 +1,11 @@
 extends Node3D
 
-
+var firepoint
+const FORCE = 100
 # Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
 
+func _ready():
+	firepoint = get_node("Camera3D/Firepoint")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -16,3 +17,15 @@ func _process(delta):
 		for otherbody in Globals.celestial_bodies:
 			if otherbody.name == "SunBody":
 				otherbody.mass -= 10.0
+	
+	if Input.is_mouse_button_pressed(1):
+		var bullet = preload("res://bullet.tscn").instantiate()
+		var level = get_node(".")
+		level.add_child(bullet)
+		bullet.set_global_transform(firepoint.global_transform)
+		bullet.apply_central_impulse(firepoint.global_transform.basis.z * -FORCE)
+		
+		
+		
+		
+		
